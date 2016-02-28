2a29666ce62a8f585e50933d55ff579dcec237bd
jimbelton <jim.belton@gmail.com>
2016-02-27 17:09:07 -0800

    Fixed test and updated output exemplar

--- test/test-wk-extract.py
+++ test/test-wk-extract.py
@@ -13,7 +13,7 @@ class TestWkExtract(unittest.TestCase):
     def testSimplify(self):
         linesOut = open(os.path.join(wikidataDir, "test/1000.out")).read().split()
         linesNew = subprocess.check_output([wdExtractPy, "-f", "-l", "en", "-s", "", os.path.join(wikidataDir, "data/1000.json")]).split()
-        diff     = list(difflib.Differ().compare(linesOut, linesNew))
+        diff     = list(difflib.context_diff(linesOut, linesNew))
         self.assertEqual(len(diff), 0, "Unexpected differences in output of wd-extract.py -f -l en -s '' data/1000.json\n" + "".join(diff))
 
 if __name__ == '__main__':
