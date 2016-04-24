be8168030b762a94ad6bbed3ecd2162002683bd6
jimbelton <jim.belton@gmail.com>
2016-04-24 01:09:53 -0700

    Filter books by label language or original language

--- wd-package.py
+++ wd-package.py
@@ -5,7 +5,7 @@
 Usage: wd-package.py [-l languages] <class> <data> <index>
 
 Options:
-    -l --languages list    Include only books in one of the comma separated list of languages (default=all). e.g. "English,German".
+    -l --languages list    Include only books in one of the comma separated list of languages (default=all). e.g. "en,de".
                            If no 'original language of work' is specified and the book does not have a label in the requested
                            language, don't include it. This requires that the extraction being used has multilingual strings ('-l'
                            was not specified) or has label languages preserved ('-L' was specified).
@@ -21,15 +21,18 @@ unwantedProperties = {
 # For each package, the map of properties that point to other objects to the names of collections of those objects.
 #
 objectProperties = {
-    "books": {"author":                     "authors",
-              "award recieved":             "awards",
-              "country of origin":          "countries",
-              "creator":                    "creators",
-              "genre":                      "genres",
-              "nominated for":              "nominations",
-              "original language of work":  "languages",
-              "publisher":                  "publishers",
-              "series":                     "series"}
+    "books": {
+        "class":                      "Q571",
+        "author":                     "authors",
+        "award recieved":             "awards",
+        "country of origin":          "countries",
+        "creator":                    "creators",
+        "genre":                      "genres",
+        "nominated for":              "nominations",
+        "original language of work":  "languages",
+        "publisher":                  "publishers",
+        "series":                     "series"
+    }
 }
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "lib"))
@@ -50,14 +53,22 @@ lineNum   = 0
 if args["<class>"][:1] == "Q":
     classIds.add(args["<class>"])
 
-elif args["<class>"] == "books":
-    classIds.add("Q571")
+elif args["<class>"] in objectProperties:
+    classIds.add(objectProperties[args["<class>"]]["class"])
 
 else:
-    sys.exit("%s: Class '%s' should be Q#### or books" % (__file__, args["<class>"]))
+    fatal("Class '%s' should be Q#### or in {%s}" % (args["<class>"], ",".join(objectProperties.keys())))
 
 itemCache = {}
-package   = {"items": {}, args["<class>"]: {}}
+package   = {"items": {}, args["<class>"]: []}
+
+if args["<class>"] in objectProperties:
+    objectProperties = objectProperties[args["<class>"]]
+else:
+    objectProperties = {}
+
+for property in objectProperties:
+    package[property] = []
 
 # Get an item, either from the package, the cache, or the extracted data
 #
@@ -95,9 +106,55 @@ for line in data:
     if classId == None:
         continue
 
-    #json.dump(object, sys.stderr, sort_keys=True, indent=4, separators=(',', ': '))
+    if "label" not in object:
+        error("Item %s is a %s but has no label" % (object["id"], args["<class>"]), file=args["<data>"], line=lineNum)
+        continue
+
+    # If we're filtering the languages
+    #
+    if languages:
+
+        # If the label's language has been preserved, use it
+        if isinstance(object["label"], dict):
+            if object["label"]["language"] not in languages:
+                warn("Item '%s' label language is '%s': skipped" % (object["label"]["value"], object["label"]["language"]),
+                     file=args["<data>"], line=lineNum)
+                continue
+
+            object["label"] = object["label"]["value"]
+
+        # Otherwise, use the original language of the work
+        elif "original language of work" in object:
+            originalLanguageIds = set()
+
+            for i, lang in enumerate(object["original language of work"]):
+                languageLabel = lang
+
+                if isinstance(lang, dict):
+                    languageLabel = getItem(lang["value"])["label"] if lang["type"] == "item" else lang["value"]
+
+                try:
+                    originalLanguageIds.add(language.nameToIsoId(languageLabel))
+                except KeyError:
+                    if languageLabel == u'n/a (silent film)':
+                        continue
+
+                    fatal("Item '%s' has unknown original language '%s'"  % (object["label"],  languageLabel),
+                        file=args["<data>"], line=lineNum)
+
+            if len(originalLanguageIds & languages) == 0:
+                try:
+                    error("Item '%s' is in language '%s'" % (object["label"], ",".join(list(originalLanguageIds))))
+                except UnicodeEncodeError:
+                    try:
+                        error("Item '%s' is in language '%s'" % (json.dumps(object["label"]), ",".join(list(originalLanguages))))
+                    except UnicodeEncodeError:
+                        error("Item '%s' is in language '%s'"
+                            % (json.dumps(object["label"]), ",".join([json.dumps(language) for language in orginalLanguages])))
+                continue
 
     # For each property; note: use extracted key to allow deletion.
+    #
     for property in object.keys():
         if property in unwantedProperties:
             del object[property]
@@ -138,40 +195,12 @@ for line in data:
                 #    error("Item '%s' property %s contains a bad object: %s" % (object["label"], property, str(value)),
                 #          file=args["<data>"], line=lineNum)
 
-    # If we're filtering the languages
-    if languages:
-        if "label" in object and isinstance(object["label"], dict):
-            if object["label"]["language"] not in languages:
-                 warn("Item '%s' label language is '%s': skipped" % (object["label"]["value"], object["label"]["language"]),
-                      file=args["<data>"], line=lineNum)
-
-        elif "original language of work" in object:
-            originalLanguageIds = set()
-
-            for lang in object["original language of work"]:
-                languageLabel = lang["value"] if isinstance(lang, dict) else lang
-
-                try:
-                    originalLanguageIds.add(language.nameToIsoId(languageLabel))
-                except KeyError:
-                    if languageLabel == u'n/a (silent film)':
-                        continue
-
-                    fatal("Item '%s' has unknown original language '%s'"  % (object["label"],  languageLabel),
-                        file=args["<data>"], line=lineNum)
-
-            if len(originalLanguageIds & languages) == 0:
-                try:
-                    error("Item '%s' is in language '%s'" % (object["label"], ",".join(list(originalLanguageIds))))
-                except UnicodeEncodeError:
-                    try:
-                        error("Item '%s' is in language '%s'" % (json.dumps(object["label"]), ",".join(list(originalLanguages))))
-                    except UnicodeEncodeError:
-                        error("Item '%s' is in language '%s'"
-                            % (json.dumps(object["label"]), ",".join([json.dumps(language) for language in orginalLanguages])))
+        if property in objectProperties:
+            pass
 
     package["items"        ][object["id"]] = object
-    package[args["<class>"]][object["id"]] = None
+    package[args["<class>"]].append(object["id"])
     del object["id"]
 
+package[args["<class>"]].sort(key=lambda id: package["items"][id]["label"])
 print json.dumps(package, sort_keys=True, indent=4, separators=(',', ': '))
